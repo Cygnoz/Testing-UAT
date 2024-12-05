fa14ca33b760d934acb0325347aab399052ec134
ThahaNex <mohd.thaha@cygnonex.com>
2024-12-05 09:13:38 +0530

    new

--- server.js
+++ server.js
@@ -34,9 +34,9 @@ server.use(organizationRouter);
 const PORT = process.env.PORT || 5004;
 
 server.get('/', (req, res) => {
-    res.status(200).json("Bill BIZZ server started - Organization");
+    res.status(200).json("SIT Bill BIZZ server started - Organization");
 });
 
 server.listen(PORT, () => {
-    console.log(`BillBIZZ server Organization started at port : ${PORT}`);
+    console.log(`SIT BillBIZZ server Organization started at port : ${PORT}`);
 });
